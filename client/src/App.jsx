bcaaaee6c2793d21ac41c1d2fb2ed6f9eea4917e
Parshva Gang <coderphilosophy09@gmail.com>
2024-02-18 19:19:49 +0530

    Add header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-function App() {
+import Header from './components/Header'
+import { 
+  About,
+  Dashboard,
+  Home,
+  Projects,
+  SignIn,
+  SignUp
+ } from "./pages"
 
+ import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
   return (
-    <>
-      <div className="bg-blue-100">hello</div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="dashboard" element={<Dashboard/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='projects' element={<Projects/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
